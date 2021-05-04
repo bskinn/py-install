b42c345ebc7206d15bc13d071d6439116e5faede
Brian Skinn <brian.skinn@gmail.com>
2021-05-04 16:27:08 -0400

    Add basic tarball sanity check
    Next is to add extraction

--- py-install.py
+++ py-install.py
@@ -4,22 +4,25 @@ import argparse as ap
 import re
 import subprocess as sp
 import sys
+import tarfile
 from urllib.error import URLError
 
 
-PAT_CORE_VERSION = re.compile(r"\d+[.]\d+[.]\d+")
+pat_version_patch = re.compile(r"\d+[.]\d+[.]\d+")
+pat_version_minor = re.compile(r"\d+[.]\d+")
 
-TARBALL_PATH: str = "https://www.python.org/ftp/python/{ver_tree}/Python-{ver_file}.tgz"
+TARBALL_URL: str = "https://www.python.org/ftp/python/{ver_tree}/Python-{ver_file}.tgz"
+TARBALL_FNAME: str = "Python-{ver_patch}.tgz"
+
+VERSION: str = "version"
+VERSION_TO_PATCH: str = "version_patch"
+VERSION_TO_MINOR: str = "version_minor"
 
 
-def download_tarball(ver):
-    try:
-        ver_tree = PAT_CORE_VERSION.match(ver).group(0)
-    except AttributeError:  # No match, getattr() on None
-        print("ERROR: Could not construct tarball download URL")
-        return False
-
-    tb_path = TARBALL_PATH.format(ver_tree=ver_tree, ver_file=ver)
+def download_tarball(params):
+    tb_path = TARBALL_URL.format(
+        ver_tree=params[VERSION_TO_PATCH], ver_file=params[VERSION]
+    )
 
     try:
         result = sp.run(
@@ -31,17 +34,32 @@ def download_tarball(ver):
             shell=True,
         )
     except sp.CalledProcessError:
-        print("\n\nERROR: Process error during tarball download")
+        print("\nERROR: Process error during tarball download")
         return False
     except sp.TimeoutExpired:
-        print("\n\nERROR: Timeout during tarball download")
+        print("\nERROR: Timeout during tarball download")
         return False
 
     return True
 
 
-def extract_tarball():
-    pass
+def check_tarball(tf):
+    """Execute basic sanity checks."""
+    breakpoint()
+    return (
+        len([name for name in tf.getnames() if not name.lower().startswith("python")])
+        == 0
+    )
+
+
+def extract_tarball(params):
+    tf = tarfile.open(TARBALL_FNAME.format(ver_patch=params[VERSION_TO_PATCH]))
+
+    if not check_tarball(tf):
+        print("ERROR: Validation failure on downloaded tarball")
+        return False
+
+    return True
 
 
 def delete_tarball():
@@ -71,16 +89,45 @@ def update_symlinks():
 def get_params():
     prs = ap.ArgumentParser(description="Automated download/build/install of CPython")
 
-    prs.add_argument("version", help="Version of CPython to install")
+    prs.add_argument(VERSION, help="Version of CPython to install")
 
     ns = prs.parse_args()
     return vars(ns)
 
 
+def generate_reduced_versions(params):
+    ver = params[VERSION]
+
+    mch_patch = pat_version_patch.match(ver)
+
+    if not mch_patch:
+        print("\nERROR: Could not extract 'major.minor.patch' version")
+        return False
+
+    ver_patch = mch_patch.group(0)
+    params[VERSION_TO_PATCH] = ver_patch
+
+    mch_minor = pat_version_minor.match(ver_patch)
+
+    if not mch_minor:
+        print("\nERROR: Could not extract 'major.minor' version")
+        return False
+
+    params[VERSION_TO_MINOR] = mch_minor.group(0)
+
+    return True
+
+
 def main():
     params = get_params()
 
-    if not download_tarball(params["version"]):
+    if not generate_reduced_versions(params):
+        return 1
+
+    if not download_tarball(params):
+        return 1
+
+    if not extract_tarball(params):
         return 1
 
 
