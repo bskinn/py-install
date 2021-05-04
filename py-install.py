626a494e825245544a61665e19c653de8d213f44
Brian Skinn <brian.skinn@gmail.com>
2021-05-04 10:03:37 -0400

    Add skeleton of script

---
+++ py-install.py
@@ -0,0 +1,38 @@
+#! /usr/bin/python3
+
+import sys
+
+def download_tarball():
+    pass
+
+def extract_tarball():
+    pass
+
+def delete_tarball():
+    pass
+
+def edit_ssl():
+    pass
+
+def run_configure():
+    pass
+
+def make_python():
+    pass
+
+def install_python():
+    pass
+
+def update_symlinks():
+    pass
+
+def get_params():
+    pass
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    sys.exit(main())
+
